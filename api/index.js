d9799427bb8c3de611a89e139d447eca8543f1dd
athuridha <amarathuridhaa@gmail.com>
2026-07-09 20:30:00 +0700

    diagnose: add try-catch error response to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,12 @@
-const app = require('../server.js');
-module.exports = app;
+try {
+  const app = require('../server.js');
+  module.exports = app;
+} catch (err) {
+  module.exports = (req, res) => {
+    res.status(500).json({
+      error: "Failed to require server.js",
+      message: err.message,
+      stack: err.stack
+    });
+  };
+}
